35ea8edd2c3fdfbcb3947edc3b906552d5507aa0
Ashoka Mahadevaiah <ashok.mahdev@gmail.com>
2018-03-05 09:12:43 -0500

    tostr and directives

--- src/app/create-event/create-event.component.ts
+++ src/app/create-event/create-event.component.ts
@@ -1,5 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,Inject,ViewContainerRef} from '@angular/core';
 import { Router } from '@angular/router';
+// import { EventService } from '../index';
+import { EventService } from '../service/event.service'
+import {ToastsManager} from 'ng2-toastr/ng2-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {TOASTER_TOKEN,Toastr} from '../common/toastr.service';
 
 @Component({
   selector: 'app-create-event',
@@ -7,10 +13,49 @@ import { Router } from '@angular/router';
   styleUrls: ['./create-event.component.css']
 })
 export class CreateEventComponent implements OnInit {
+  event: any;
 
-  constructor( private router : Router) { }
+  isDirty:boolean = true;
+  
+      //   constructor(public toastr: ToastsManager, vcr: ViewContainerRef) {
+      //    this.toastr.setRootViewContainerRef(vcr);
+      // }
+      
+  // constructor( private router : Router, private eventService : EventService,public toastr: ToastsManager, vcr: ViewContainerRef) { 
+  //    this.toastr.setRootViewContainerRef(vcr);
+  // }
+  
+   constructor( private router : Router, private eventService : EventService,
+   @Inject(TOASTER_TOKEN) private toastr: Toastr) { 
+     
+  }
 
   ngOnInit() {
+    this.event = {
+      name: 'NG spectacular',
+      date: '01/25/2018',
+      time: '10AM',
+      price: 500,
+      location:{
+        address:'Hartford Main Street',
+        city: 'Hartford',
+        country: 'USA'
+      },
+      onlineUrl: 'http://ngspectaculr.com',
+      imageUrl: 'http://ngspectaculr.com/logo.png'
+    };
+  }
+
+  saveEvent(formValues){
+    // console.log(formValues);
+    this.eventService.saveEvent(formValues);
+    this.isDirty = false;
+    //console.log( this.toastr)
+    this.toastr.success('You are awesome!', 'Success!');
+    this.toastr.error('There was an Error', 'Error!');
+    this.toastr.info('Events saved', 'Info!');
+     this.toastr.warning('You are about to save', 'warning!');
+     //this.router.navigate(['/events']);
   }
 
   cancel(){
